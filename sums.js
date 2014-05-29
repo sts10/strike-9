3ab95ed72d10c93d9f0bf8dd00a2d42271bd473d
sts10 <sschlinkert@gmail.com>
2014-05-29 12:59:11 -0400

    no duplicates, and everything get sorted

--- sums.js
+++ sums.js
@@ -1,10 +1,13 @@
 var combinations = [];
 
 function getPossSums(sum){
+  
+
   for (var i = 1; i < sum/2; i++){ 
     combinations.push([i, sum-i]);
     getAllSubSums([i], sum-i);
   }
+
   combinations.push(sum); // can just play the number itself 
 
   return combinations;
@@ -14,22 +17,51 @@ function getPossSums(sum){
 function getAllSubSums(baggage, sum){
   for (var j=1; j < sum/2; j++){
     // if j is not in baggage and sum-j is not in baggage
+    if (baggage.indexOf(j) == -1 && baggage.indexOf(sum-j) == -1){
       // add j and sum-j back to baggage
       var new_array = baggage.concat(j, sum-j);
       // and push all of that to combinations
-      combinations.push(new_array);
+
+      // if new_array is NOT a subArray of combinations yet... 
+      if (!isSubArray(new_array,combinations)){
+        combinations.push(new_array);
+      }
 
       var new_baggage = baggage.concat(j);
       getAllSubSums(new_baggage, sum-j);
+    }
   }
 
 }
 
 
+function sortArray(array){
+  return array.sort(function(a, b){return a-b});
+}
 
+function sortAllSubArrays(array){
+  for(var i = 0; i < array.length; i++){
+    array[i] = sortArray(array[i]);
+  }
+  return array;
+}
 
+function isSubArray (subArray, array) {
+  subArray = sortArray(subArray);
+  array = sortAllSubArrays(array);
 
+  for(var i = 0; i < array.length; i++) {
+    if(subArray.toString() === array[i].toString()){
+      return true;
+    }
+  }
+  return false;
+}
 
-print(getPossSums(5).join('\n'));
+// print(sortArray([1,90,4,189391,0,2]));
+
+// print(isSubArray([1,2], [[2,1],[3,4]]));
+
+print(getPossSums(7).join('\n'));
 
 // => [[1,9],[2,8],[3,7],[4,6], [1,4,5], [1,6,3]
